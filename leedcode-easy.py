dd143b8629e47df3e36d612b24418bfbf265bb7d
Josephus Bawah <jbawah002@st.ug.edu.gh>
2026-06-14 22:29:12 +0000

    326. Power of Three

--- leedcode-easy.py
+++ leedcode-easy.py
@@ -137,3 +137,16 @@ class Solution:
             else:
                 answer.append(str(i))
         return answer
+# 326. Power of Three
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n <= 0:
+            return False
+
+        i = 0
+        while 3**i <= n:
+            if 3**i == n:
+                return True
+            i += 1
+
+        return False
