986713d9e49d49339b3fafa538b240a360b2326a
Josephus Bawah <jbawah002@st.ug.edu.gh>
2026-06-16 13:00:06 +0000

    509. Fibonacci Number

--- leedcode-easy.py
+++ leedcode-easy.py
@@ -150,3 +150,10 @@ class Solution:
             i += 1
 
         return False
+
+# 509. Fibonacci Number
+class Solution:
+    def fib(self, n: int) -> int:
+        if n==0 or n==1:
+            return n
+        return self.fib(n-1)+self.fib(n-2)
