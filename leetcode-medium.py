002263bbcb7e6fcda789108ca4f94dfd82b4b317
Josephus Bawah <jbawah002@st.ug.edu.gh>
2026-06-16 13:35:18 +0000

    3612. Process String with Special Operations I

--- leetcode-medium.py
+++ leetcode-medium.py
@@ -19,3 +19,21 @@ class Solution:
 class Solution:
     def maxTotalValue(self, nums: List[int], k: int) -> int:
         return (max(nums)-min(nums))*k
+
+# 3612. Process String with Special Operations I
+class Solution:
+    def processStr(self, s: str) -> str:
+        import string
+        result=[]
+        for i in s:
+            if i in list(string.ascii_lowercase):
+                result.append(i)
+            elif i=='*':
+                if len(result)>0:
+                    result.pop(-1)
+            elif i=='#':
+                cp=result.copy()
+                result.extend(cp)
+            elif i=='%':
+                result.reverse()
+        return ''.join(result)
