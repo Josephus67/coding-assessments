6f135d935189ad3b5a4335211d3a5643600218ad
Josephus Bawah <jbawah002@st.ug.edu.gh>
2026-06-18 13:52:43 +0000

    1344. Angle Between Hands of a Clock

--- leetcode-medium.py
+++ leetcode-medium.py
@@ -47,3 +47,14 @@ class Solution:
             if b in look:
                 return i
         return -1
+
+# 1344. Angle Between Hands of a Clock 
+class Solution:
+    def angleClock(self, hour: int, minutes: int) -> float:
+        minute_angle = minutes * 6
+        
+        hour_angle = (hour % 12) * 30 + minutes * 0.5
+        
+        diff = abs(hour_angle - minute_angle)
+        
+        return min(diff, 360 - diff)
