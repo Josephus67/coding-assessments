663ef47587cc7b9b340d90e88e9befc31dfc4793
Josephus Bawah <jbawah002@st.ug.edu.gh>
2026-06-28 19:41:34 +0000

    1846. Maximum Element After Decreasing and Rearranging

--- leetcode-medium.py
+++ leetcode-medium.py
@@ -86,3 +86,14 @@ class Solution:
                 stack.append(char)
                 
         return not stack
+
+# 1846. Maximum Element After Decreasing and Rearranging   
+class Solution:
+    def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
+        arr.sort()
+        arr[0] = 1
+        for i in range(1, len(arr)):
+            if arr[i] > arr[i - 1] + 1:
+                arr[i] = arr[i - 1] + 1  
+        return arr[-1]
+
